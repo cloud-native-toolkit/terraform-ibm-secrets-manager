53708c98f7e88e884db80a6ab4744ebc1046db87
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2023-01-19 16:23:54 +1100

    Cleans up unused variables
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage2-secrets-manager.tf
+++ test/stages/stage2-secrets-manager.tf
@@ -7,10 +7,7 @@ module "secrets-manager" {
   private_endpoint = false
   kms_private_endpoint = true
   kms_enabled = true
-  kms_id      = module.kms_key.kms_id
   kms_key_crn = module.kms_key.crn
-  kms_private_url = module.kms_key.kms_private_url
-  kms_public_url  = module.kms_key.kms_public_url
   ibmcloud_api_key = var.ibmcloud_api_key
   name_prefix = var.name_prefix
   trial = true
